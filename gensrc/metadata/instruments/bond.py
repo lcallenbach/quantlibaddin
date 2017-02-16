641163d0be069f35ab29cdf8efdbbbbd19cb434d
Pepe <japari@free.fr>
2017-02-16 10:46:26 +0100

    add some functions

---
+++ gensrc/metadata/instruments/bond.py
@@ -0,0 +1,10 @@
+
+QuantLibTypes = [
+   ("MemberFunction", "ql/instruments/bond.hpp",
+        "QuantLib::Bond()", "QuantLib::Real cleanPrice()",
+        "qlBondCleanPrice"),
+    ("MemberFunction", "ql/instruments/bond.hpp",
+        "QuantLib::Bond()", "QuantLib::Real dirtyPrice()",
+        "qlBondDirtyPrice")
+]
+
