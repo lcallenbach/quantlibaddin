674cdefac0d9539fca4eb79d8927c74b57737aee
Pepe <japari@free.fr>
2017-02-20 21:41:23 +0100

    Bond functions added, but bond type to zspread no working

---
+++ gensrc/metadata/compounding.py
@@ -0,0 +1,10 @@
+ImplicitConversionsCalc = [
+    ("QuantLib::Compounding",  "std::string", "string", "EnumeratedType", "<<"),
+]
+
+QuantLibTypes = [
+    ("EnumeratedType", "ql/compounding.hpp", "QuantLib::Compounding", "std::string", "Simple", "QuantLib::Simple"),
+    ("EnumeratedType", "ql/compounding.hpp", "QuantLib::Compounding", "std::string", "Compounded", "QuantLib::Compounded"),
+    ("EnumeratedType", "ql/compounding.hpp", "QuantLib::Compounding", "std::string", "Continuous", "QuantLib::Continuous"),
+    ("EnumeratedType", "ql/compounding.hpp", "QuantLib::Compounding", "std::string", "SimpleThenCompounded", "QuantLib::SimpleThenCompounded"),
+]
--- gensrc/metadata/instruments/bond.py
+++ gensrc/metadata/instruments/bond.py
@@ -1,4 +1,12 @@
 
+# intended just to have the type:
+
+QuantLibTypes = [
+    ("Constructor", "ql/instruments/bond.hpp", "QuantLib::Bond(QuantLib::Natural settlementDays, QuantLib::Calendar calendar, QuantLib::Date issueDate = QuantLib::Date(), QuantLib::Leg coupons = QuantLib::Leg())", "", "qlBond"),
+]
+
+
+
 QuantLibTypes = [
    ("MemberFunction", "ql/instruments/bond.hpp",
         "QuantLib::Bond()", "QuantLib::Real cleanPrice()",
